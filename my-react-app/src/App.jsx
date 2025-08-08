cfc25a4bd19ee47ffa9bbf5d1d87963038286265
muhammednavafbinsyd <nokocon374@deenur.com>
2025-08-08 17:30:51 +0530

    initial commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-md">
+        <h1 className="">
+          Hello Tailwind CSS v4!
+        </h1>
+        <h1 className=''></h1>
+        <p className="text-gray-600">
+          Your Vite + React + Tailwind CSS v4 setup is working!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
